300f77261197967e883e4c7384d930b57e105db3
edwardjay2 <120265419+edwardjay2@users.noreply.github.com>
2026-04-10 05:44:54 -0700

    Add button control for LED in button-control.ino
    Added basic button control for LED ON and OFF

---
+++ arduino-basics/blink-led/button-control/button-control.ino
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+void setup() {
+  pinMode(2, INPUT);
+  pinMode(3, OUTPUT);
+}
+
+void loop() {
+  int button = digitalRead(2);
+
+  if (button == HIGH) {
+    digitalWrite(3, HIGH);
+  } else {
+    digitalWrite(3, LOW);
+  }
+}
