14e687a703319b2752d2bbbf8869fd90f0853701
edwardjay2 <120265419+edwardjay2@users.noreply.github.com>
2026-04-18 07:02:42 -0700

    Implement dual LED sequence control
    Added dual LED sequence control to blink-led.ino.

--- blink-led/blink-led.ino
+++ blink-led/blink-led.ino
@@ -1,3 +1,5 @@
+#include <Arduino.h>
+
 void setup() {
     pinMode(2, OUTPUT); // Set built-in LED pin as output
 }
@@ -7,4 +9,26 @@ void loop() {
     delay(1000);            // Wait 1 second
     digitalWrite(2, LOW);   // Turn LED off
     delay(1000);            // Wait 1 second
+}
+
+======================== DUAL LED SEQUENCE CONTROL =========================
+#include <Arduino.h>
+
+void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(2, HIGH);
+  delay(1000);
+
+  digitalWrite(3, HIGH);
+  delay(2000);
+
+  digitalWrite(2, LOW);
+  delay(1000);
+
+  digitalWrite(3, LOW);
+  delay(2000);
 }
