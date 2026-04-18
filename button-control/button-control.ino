fff6d39d8cdd2e3908dc5446a108d0a5387fbf2e
edwardjay2 <120265419+edwardjay2@users.noreply.github.com>
2026-04-18 06:44:21 -0700

    Button control

--- button-control/button-control.ino
+++ button-control/button-control.ino
@@ -15,20 +15,4 @@ void loop() {
   }
 }
 
-//============== VERSION 2:REVERSE(LED ON WHEN NOT PRESSED)===============
-#include <Arduino.h>
 
-void setup() {
-  pinMode(2, INPUT);
-  pinMode(3, OUTPUT);
-}
-
-void loop() {
-  int button = digitalRead(2);
-
-  if (button == LOW) {   // Changed from HIGH to LOW
-    digitalWrite(3, HIGH);  // LED ON when button NOT pressed
-  } else {
-    digitalWrite(3, LOW);   // LED OFF when button is pressed
-  }
-}
