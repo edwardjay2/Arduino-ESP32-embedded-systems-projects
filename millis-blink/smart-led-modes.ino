4529a455954ed9dcc3cf1eccf56c9c1964c88a2e
edwardjay2 <120265419+edwardjay2@users.noreply.github.com>
2026-04-18 06:20:43 -0700

    Add smart LED modes with serial input handling
    Implement smart LED control with user input for modes.

--- millis-blink/smart-led-modes.ino
+++ millis-blink/smart-led-modes.ino
@@ -1,1 +1,65 @@
+#include <Arduino.h>
 
+//Timing intervals
+unsigned long fastInterval = 300;
+unsigned long slowInterval = 1000;
+
+//Variables
+unsigned long previousTime = 0;
+unsigned long currentInterval = fastInterval;
+
+
+bool ledState = false;
+bool systemOn = true;
+bool alwaysOn = false;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Smart LED Controller Started");
+  Serial.println("Press 1 = FAST, 2 = SLOW, 3 = ALWAYS ON, 0 = OFF");
+}
+
+
+void loop() {
+  unsigned long currentMillis = millis();
+
+  //  HANDLE USER INPUT
+  if(Serial.available() > 0) {
+    char input = Serial.read();
+
+    if (input == '1') {
+      currentInterval = fastInterval;
+      systemOn = true;
+      Serial.println("Mode: FAST");
+    }
+    else if (input == '2') {
+      currentInterval = slowInterval;
+      systemOn = true;
+      Serial.println("Mode: SLOW");
+    }
+    else if (input == '3') {
+      alwaysOn = true;
+      systemOn = true;
+      Serial.println("Mode: ALWAYS ON ");
+    }
+     else if (input == '0') {
+      systemOn = false;
+      Serial.println("LED OFF");
+    }
+  }
+   //ALWAYS ON MODE (no blinking)
+   if (alwaysOn && systemOn) {
+    Serial.println("LED is ON (STATIC)");
+    delay(500); // small delay to avoid spam
+   }
+
+  //  NON-BLOCKING LED CONTROL
+  if (systemOn && (currentMillis - previousTime >= currentInterval)) {
+    previousTime = currentMillis;
+    ledState = !ledState;
+
+    Serial.print("LED is");
+    Serial.println(ledState ? "ON" : "OFF");
+  }
+  
+}
