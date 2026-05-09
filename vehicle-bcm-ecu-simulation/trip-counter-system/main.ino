0a91e0df5082fdfa7c0ea91d7e2694d4b8810d78
Nkwuzor Edward Nnaemeka <120265419+edwardjay2@users.noreply.github.com>
2026-05-09 09:37:59 -0700

    Implement trip counter with EEPROM storage

--- vehicle-bcm-ecu-simulation/trip-counter-system/main.ino
+++ vehicle-bcm-ecu-simulation/trip-counter-system/main.ino
@@ -1,1 +1,42 @@
+#include <Arduino.h>
+#include <EEPROM.h>
 
+const int EEPROM_SIZE = 32;
+int tripCount = 0;
+
+
+void setup(){
+  Serial.begin(9600);
+  EEPROM.begin(EEPROM_SIZE);
+  Serial.println("====== TRUCK STORED TRIP COUNTER ======");
+  Serial.println("S = STORE TRIP COUNT");
+  Serial.println("R = READ SAVED TRIPS");
+
+  tripCount = EEPROM.read(0);
+
+}
+
+void loop(){
+  if(Serial.available () > 0){
+    char input = Serial.read();
+
+    if(input == 'S'){
+      tripCount++;
+
+      EEPROM.write(0, tripCount);
+      EEPROM.commit();
+
+      Serial.print("Trip Count:");
+      Serial.println(tripCount);
+    }
+    else if(input == 'R'){
+      tripCount = EEPROM.read(0);
+
+      Serial.print("Saved Trips: ");
+      Serial.println(tripCount);
+
+
+    }
+  }
+
+}
