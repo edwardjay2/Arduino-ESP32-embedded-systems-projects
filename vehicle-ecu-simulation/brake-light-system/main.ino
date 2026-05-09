9f8d10796ab995ee3a47161d2a3bffdd3eaefa2e
Nkwuzor Edward Nnaemeka <120265419+edwardjay2@users.noreply.github.com>
2026-05-09 04:52:29 -0700

    Implement brake light system in main.ino

--- vehicle-ecu-simulation/brake-light-system/main.ino
+++ vehicle-ecu-simulation/brake-light-system/main.ino
@@ -1,1 +1,19 @@
+#include <Arduino.h>
 
+const int brakeSwitch = 2;
+const int brakeLight = 3;
+
+void setup() {
+  pinMode(brakeSwitch, INPUT_PULLUP);
+  pinMode(brakeLight, OUTPUT);
+}
+
+void loop() {
+  int brakeState = digitalRead(brakeSwitch);
+
+  if (brakeState == LOW) {
+    digitalWrite(brakeLight, HIGH); // Brake applied
+  } else {
+    digitalWrite(brakeLight, LOW);  // Brake released
+  }
+}
