ff9b864253efb40fcab1e5fedf317c3b770f43b1
Nkwuzor Edward Nnaemeka <120265419+edwardjay2@users.noreply.github.com>
2026-05-09 09:43:43 -0700

    Refactor trip counter system messages and logic

--- vehicle-bcm-ecu-simulation/trip-counter-system/main.ino
+++ vehicle-bcm-ecu-simulation/trip-counter-system/main.ino
@@ -4,39 +4,49 @@
 const int EEPROM_SIZE = 32;
 int tripCount = 0;
 
-
-void setup(){
+void setup() {
   Serial.begin(9600);
   EEPROM.begin(EEPROM_SIZE);
-  Serial.println("====== TRUCK STORED TRIP COUNTER ======");
+
+  Serial.println("==== SMART TRUCK TRIP COUNTER SYSTEM ====");
   Serial.println("S = STORE TRIP COUNT");
-  Serial.println("R = READ SAVED TRIPS");
+  Serial.println("R = READ SAVED TRIP");
+  Serial.println("C = RESET TRIP COUNT");
 
   tripCount = EEPROM.read(0);
-
 }
 
-void loop(){
-  if(Serial.available () > 0){
+void loop() {
+
+  if (Serial.available() > 0) {
+
     char input = Serial.read();
 
-    if(input == 'S'){
+    if (input == 'S') {
       tripCount++;
 
       EEPROM.write(0, tripCount);
       EEPROM.commit();
 
-      Serial.print("Trip Count:");
+      Serial.print("Trip Count: ");
       Serial.println(tripCount);
     }
-    else if(input == 'R'){
+
+    else if (input == 'R') {
       tripCount = EEPROM.read(0);
 
       Serial.print("Saved Trips: ");
       Serial.println(tripCount);
+    }
 
+    else if (input == 'C') {
 
+      tripCount = 0;
+
+      EEPROM.write(0, 0);
+      EEPROM.commit();
+
+      Serial.println("TRIP COUNTER RESET");
     }
   }
-
 }
