580d473ecb951d4ca50afa45cdb3b3f2af35d225
Nkwuzor Edward Nnaemeka <120265419+edwardjay2@users.noreply.github.com>
2026-05-09 05:10:44 -0700

    Implement fan control based on pressure and temperature

--- vehicle-ecu-simulation/fan-control-system/main.ino
+++ vehicle-ecu-simulation/fan-control-system/main.ino
@@ -1,1 +1,27 @@
+#include <Arduino.h>
 
+const int pressurePin = 2;
+const int tempPin = 4;
+const int fanOutput = 3;
+
+bool fanState = false;
+
+void setup() {
+  pinMode(pressurePin, INPUT_PULLUP);
+  pinMode(tempPin, INPUT_PULLUP);
+  pinMode(fanOutput, OUTPUT);
+}
+
+void loop() {
+  int lowPressure = digitalRead(pressurePin);
+  int tempHot = digitalRead(tempPin);
+
+  if (tempHot == LOW || lowPressure == LOW) {
+    digitalWrite(fanOutput, HIGH); // ECU: activate fan warning
+    fanState = true;
+  } 
+  else {
+    digitalWrite(fanOutput, LOW);
+    fanState = false;
+  }
+}
