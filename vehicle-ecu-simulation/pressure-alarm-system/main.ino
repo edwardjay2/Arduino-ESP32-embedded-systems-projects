d2c116217e9eca181c3cd130d03f1d44f8d9784b
Nkwuzor Edward Nnaemeka <120265419+edwardjay2@users.noreply.github.com>
2026-05-09 05:02:03 -0700

    Implement pressure alarm system in main.ino

--- vehicle-ecu-simulation/pressure-alarm-system/main.ino
+++ vehicle-ecu-simulation/pressure-alarm-system/main.ino
@@ -1,1 +1,32 @@
+#include <Arduino.h>
 
+const int pressurePin = 2;
+const int alarmLed = 3;
+
+unsigned long previousTime = 0;
+const unsigned long interval = 1000;
+
+bool alarmState = false;
+
+void setup() {
+  pinMode(pressurePin, INPUT_PULLUP);
+  pinMode(alarmLed, OUTPUT);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  int pressureState = digitalRead(pressurePin);
+
+  if (pressureState == LOW) { // low pressure detected
+    if (currentMillis - previousTime >= interval) {
+      previousTime = currentMillis;
+
+      alarmState = !alarmState; // toggle LED
+      digitalWrite(alarmLed, alarmState);
+    }
+  } 
+  else {
+    digitalWrite(alarmLed, LOW);
+    alarmState = false;
+  }
+}
