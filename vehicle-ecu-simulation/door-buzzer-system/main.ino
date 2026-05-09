6e880ec44db90ac1e946ae68d197d1a1170826ac
Nkwuzor Edward Nnaemeka <120265419+edwardjay2@users.noreply.github.com>
2026-05-09 04:56:20 -0700

    Implement door buzzer system with Arduino

--- vehicle-ecu-simulation/door-buzzer-system/main.ino
+++ vehicle-ecu-simulation/door-buzzer-system/main.ino
@@ -1,1 +1,23 @@
+#include <Arduino.h>
 
+const int doorSwitch = 2;
+const int buzzer = 3;
+
+bool buzzerState = false;
+
+void setup() {
+  pinMode(doorSwitch, INPUT_PULLUP);
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop() {
+  int doorState = digitalRead(doorSwitch);
+
+  if (doorState == LOW) {
+    digitalWrite(buzzer, HIGH);   // Door open warning ON
+    buzzerState = true;
+  } else {
+    digitalWrite(buzzer, LOW);    // Door closed warning OFF
+    buzzerState = false;
+  }
+}
