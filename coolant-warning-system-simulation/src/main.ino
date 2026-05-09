ef5e9d272126f60549146e5ebeab42f93580d524
Nkwuzor Edward Nnaemeka <120265419+edwardjay2@users.noreply.github.com>
2026-05-09 04:33:44 -0700

    Implement coolant warning system with LED indicator

--- coolant-warning-system-simulation/src/main.ino
+++ coolant-warning-system-simulation/src/main.ino
@@ -1,1 +1,19 @@
+#include <Arduino.h>
 
+const int coolantPin = 2;
+const int warningLed = 3;
+
+void setup() {
+  pinMode(coolantPin, INPUT_PULLUP);
+  pinMode(warningLed, OUTPUT);
+}
+
+void loop() {
+  int coolantState = digitalRead(coolantPin);
+
+  if (coolantState == LOW) {
+    digitalWrite(warningLed, HIGH); // ECU: fault detected
+  } else {
+    digitalWrite(warningLed, LOW);  // ECU: normal condition
+  }
+}
