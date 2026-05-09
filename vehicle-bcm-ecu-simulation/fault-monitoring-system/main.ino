ff6cfaeb4d459ec356d5e0b51f3f5ed72802023b
Nkwuzor Edward Nnaemeka <120265419+edwardjay2@users.noreply.github.com>
2026-05-09 05:21:18 -0700

    Implement fault monitoring for vehicle components

--- vehicle-bcm-ecu-simulation/fault-monitoring-system/main.ino
+++ vehicle-bcm-ecu-simulation/fault-monitoring-system/main.ino
@@ -1,1 +1,33 @@
+#include <Arduino.h>
 
+const int coolantPin = 2;
+const int brakePin = 4;
+const int doorPin = 5;
+const int pressurePin = 6;
+
+const int warningOutput = 3;
+
+void setup() {
+  pinMode(coolantPin, INPUT_PULLUP);
+  pinMode(brakePin, INPUT_PULLUP);
+  pinMode(doorPin, INPUT_PULLUP);
+  pinMode(pressurePin, INPUT_PULLUP);
+
+  pinMode(warningOutput, OUTPUT);
+}
+
+void loop() {
+
+  bool coolantFault = (digitalRead(coolantPin) == LOW);
+  bool brakeFault = (digitalRead(brakePin) == LOW);
+  bool doorFault = (digitalRead(doorPin) == LOW);
+  bool pressureFault = (digitalRead(pressurePin) == LOW);
+
+  bool systemFault =
+      coolantFault ||
+      brakeFault ||
+      doorFault ||
+      pressureFault;
+
+  digitalWrite(warningOutput, systemFault);
+}
