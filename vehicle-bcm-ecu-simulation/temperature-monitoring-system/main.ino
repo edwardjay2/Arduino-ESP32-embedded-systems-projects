d4cdac0936cfb1aef297aaa0da9a90d1267f04bf
Nkwuzor Edward Nnaemeka <120265419+edwardjay2@users.noreply.github.com>
2026-05-09 08:58:30 -0700

    Implement temperature monitoring with EEPROM storage

--- vehicle-bcm-ecu-simulation/temperature-monitoring-system/main.ino
+++ vehicle-bcm-ecu-simulation/temperature-monitoring-system/main.ino
@@ -1,1 +1,40 @@
+#include <Arduino.h>
+#include <EEPROM.h>
 
+int temperature = 0;
+
+void setup() {
+  Serial.begin(9600);
+  EEPROM.begin(32);
+
+  Serial.println("TRUCK TEMPERATURE STORAGE");
+  Serial.println("S: SAVE TEMPERATURE");
+  Serial.println("R: PRINT TEMPERATURE");
+
+  temperature = EEPROM.read(1);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char input = Serial.read();
+
+    if (input == 'S') {
+      temperature = 45;
+      EEPROM.write(1, temperature);
+      EEPROM.commit();
+
+      Serial.println("Temperature Saved");
+    }
+
+    else if (input == 'R') {
+      temperature = EEPROM.read(1);
+
+      Serial.print("LAST TEMP: ");
+      Serial.println(temperature);
+
+      if (temperature > 40) {
+        Serial.println("WARNING: OVERHEAT");
+      }
+    }
+  }
+}
