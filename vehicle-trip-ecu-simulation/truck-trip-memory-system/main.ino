0578af356632def2e5b5fa5e67eb2dfb2f6047a7
Nkwuzor Edward Nnaemeka <120265419+edwardjay2@users.noreply.github.com>
2026-06-06 06:22:42 -0700

    Add trip count and distance logging functionality
    Implement trip counting and EEPROM storage for truck trips.

--- vehicle-trip-ecu-simulation/truck-trip-memory-system/main.ino
+++ vehicle-trip-ecu-simulation/truck-trip-memory-system/main.ino
@@ -1,1 +1,63 @@
+#include <Arduino.h>
+#include <EEPROM.h>
 
+const int EEPROM_SIZE = 32;
+int tripCount = 0;
+
+void setup() {
+  Serial.begin(9600);
+  EEPROM.begin(EEPROM_SIZE);
+
+  Serial.println("=== SMART TRUCK TRIP COUNT AND DISTANCE ===");
+  Serial.println("S = SAVE TRIP");
+  Serial.println("R = READ LAST TRIP");
+  Serial.println("C = RESET");
+
+  tripCount = EEPROM.read(0);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char input = Serial.read();
+
+    // ===== SAVE =====
+    if (input == 'S') {
+      tripCount++;
+
+      int distance = 120;
+
+      EEPROM.write(0, tripCount);
+      EEPROM.write(1, distance);
+      EEPROM.commit();
+
+      Serial.print("Trip ");
+      Serial.print(tripCount);
+      Serial.print(" -> Distance: ");
+      Serial.print(distance);
+      Serial.println("km");
+    }
+
+    // ===== READ =====
+    else if (input == 'R') {
+      int savedTrip = EEPROM.read(0);
+      int distance = EEPROM.read(1);
+
+      Serial.print("Trip ");
+      Serial.print(savedTrip);
+      Serial.print(" -> Distance: ");
+      Serial.print(distance);
+      Serial.println("km");
+    }
+
+    // ===== RESET =====
+    else if (input == 'C') {
+      tripCount = 0;
+
+      EEPROM.write(0, 0);
+      EEPROM.write(1, 0);
+      EEPROM.commit();
+
+      Serial.println("TRIP RESET");
+    }
+  }
+}
