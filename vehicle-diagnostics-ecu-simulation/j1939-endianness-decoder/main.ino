76c765f70d9f9506ed230b41c035b40f16b58819
Nkwuzor Edward Nnaemeka <120265419+edwardjay2@users.noreply.github.com>
2026-07-25 10:13:10 -0700

    Add endianness decoding for RPM values

--- vehicle-diagnostics-ecu-simulation/j1939-endianness-decoder/main.ino
+++ vehicle-diagnostics-ecu-simulation/j1939-endianness-decoder/main.ino
@@ -1,1 +1,15 @@
+uint8_t byte0 = 0xDC;
+uint8_t byte1 = 0X05;
+
+// Little Endian
+int rpmLittle = (byte1 << 8) | byte0;
+
+// Big Endian
+int rpmBig = (byte0 << 8) | byte1;
+
+Serial.print("Little Endian RPM: ");
+Serial.println(rpmLittle);
+
+Serial.print("Big Endian RPM: ");
+Serial.println(rpmBig);
 
